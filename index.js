01079503c563b5f786d2605a0b2de9671a3e4dd5
fantasyni <fantasyni@163.com>
2015-04-23 11:05:17 +0800

    upgrade to 0.1.0

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var HaClient = require('./lib/haClient');
-var MockHaClient = require('./test/haClient');
 
 var bearcatHa = {};
 
 bearcatHa.createClient = function(opts) {
-	// return new HaClient(opts);
-	return new MockHaClient(opts);
+	return new HaClient(opts);
 }
+
+bearcatHa.HASTATE = require('./lib/util/haClientState');
 
 module.exports = bearcatHa;
